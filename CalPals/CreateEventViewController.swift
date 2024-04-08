ce74049c7fc9228c8e1d3790d7f4ec8ef6e22960
emmelouwho <emilyerwin20@gmail.com>
2024-04-07 23:17:41 -0500

    fix new event page and event handling

--- CalPals/CreateEventViewController.swift
+++ CalPals/CreateEventViewController.swift
@@ -15,16 +15,26 @@ class CreateEventViewController: UIViewController, LocationChanger {
     @IBOutlet weak var latestLabel: UILabel!
     @IBOutlet weak var earliestLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
     
     // MARK: - buttons
     @IBOutlet weak var groupButton: UIButton!
-    @IBOutlet weak var noLaterButton: UIButton!
-    @IBOutlet weak var noEarlierButton: UIButton!
+    @IBOutlet weak var earliestButton: UIButton!
+    @IBOutlet weak var latestButton: UIButton!
+    @IBOutlet weak var durationButton: UIButton!
     
     //MARK: - text fields
     @IBOutlet weak var eventNameTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
-    @IBOutlet weak var durationTextField: UITextField!
+    
+    //MARK: - switches
+    @IBOutlet weak var mondaySwitch: UISwitch!
+    @IBOutlet weak var tuesdaySwitch: UISwitch!
+    @IBOutlet weak var wednesdaySwitch: UISwitch!
+    @IBOutlet weak var thursdaySwitch: UISwitch!
+    @IBOutlet weak var fridaySwitch: UISwitch!
+    @IBOutlet weak var saturdaySwitch: UISwitch!
+    @IBOutlet weak var sundaySwitch: UISwitch!
     
     // MARK: - main
     override func viewDidLoad() {
@@ -33,58 +43,84 @@ class CreateEventViewController: UIViewController, LocationChanger {
         // reseting all info
         eventNameTextField.text = ""
         descriptionTextField.text = ""
-        durationTextField.text = ""
         locationLabel.text = "Location"
-        groupButton.setTitle("", for: .normal)
-        
-        noLaterButton.setTitle(formatTime(date: Date()), for: .normal)
-        noEarlierButton.setTitle(formatTime(date: Date()), for: .normal)
         
         // styling added
         addBorder(label: groupLabel)
         addBorder(label: latestLabel)
         addBorder(label: earliestLabel)
         addBorder(label: locationLabel)
+        addBorder(label: durationLabel)
+        
+        // all menu button set up
+        timeConstaintsSetUp(defaultTime: "12AM", button: earliestButton, optionClosure: {(action: UIAction) in self.newEvent.noEarlierThan = action.title})
+        timeConstaintsSetUp(defaultTime: "11PM", button: latestButton,  optionClosure: {(action: UIAction) in self.newEvent.noLaterThan = action.title})
+        groupSetUp()
+        durationSetUp()
     }
     
+    //MARK: - setup and formatting
     func addBorder(label: UILabel){
         label.layer.borderWidth = 1.0
         label.layer.borderColor = UIColor.systemGray6.cgColor
         label.layer.cornerRadius = 5.0
         label.clipsToBounds = true
     }
+    
+    func timeConstaintsSetUp(defaultTime: String, button: UIButton, optionClosure: @escaping UIActionHandler) {
+        let actions = allTimes.reversed().map { title in
+            if title == defaultTime {
+                UIAction(title: title, state: .on, handler: optionClosure)
+            } else {
+                UIAction(title: title, handler: optionClosure)
+            }
+        }
+        
+        button.menu = UIMenu(children: actions)
+    }
+    
+    func groupSetUp() {
+        let optionClosure = {(action: UIAction) in print(action.title)}
+        let groupOptions = ["group 1", "group 2", "group 3"]
+        let actions = groupOptions.map { title in
+            UIAction(title: title, handler: optionClosure)
+        }
+        
+        groupButton.menu = UIMenu(children: actions)
+    }
+    
+    func durationSetUp() {
+        let optionClosure = {(action: UIAction) in print(action.title)}
+        
+        var timeList: [String] = []
+        for hour in 0..<48 {
+            if hour % 2 == 0 {
+                if hour > 0 {
+                    timeList.append("\(hour/2) hr")
+                }
+            } else {
+                if hour > 1 {
+                    timeList.append("\(hour/2) hr 30 min")
+                } else {
+                    timeList.append("30 min")
+                }
+            }
+        }
+        
+        let durationOptions = timeList.reversed().map {  title in
+            if title == "30 min" {
+                UIAction(title: title, state: .on, handler: optionClosure)
+            } else {
+                UIAction(title: title, handler: optionClosure)
+            }
+        }
+        durationButton.menu = UIMenu(children: durationOptions)
+    }
 
     // MARK: - Button Pressed
-    @IBAction func groupButtonPressed(_ sender: Any) {
-        let controller = UIAlertController(title: "Select group", message: "Please choose a group for this event to be assigned to", preferredStyle: .actionSheet)
-        
-        //TODO: replace once state is added for each user
-        let groupOptions = ["group 1", "group 2", "group 3"]
-        for group in groupOptions {
-            controller.addAction(UIAlertAction(title: group, style: .default, handler: {action in self.groupButton.setTitle(action.title, for: .normal)}))
-        }
-        present(controller,animated: true)
-    }
-
-    
-    @IBAction func noEarlierButtonPressed(_ sender: Any) {
-        let datePicker = UIDatePicker()
-        datePicker.datePickerMode = .time
-        datePicker.addTarget(self, action: #selector(noEarlierDateChange(datePicker: )), for: UIControl.Event.valueChanged)
-        let controller = getDateAlert(datePicker: datePicker)
-        present(controller, animated: true, completion: nil)
-    }
-    
-    @IBAction func noLaterButtonPressed(_ sender: Any) {
-        let datePicker = UIDatePicker()
-        datePicker.datePickerMode = .time
-        datePicker.addTarget(self, action: #selector(noLaterDateChange(datePicker: )), for: UIControl.Event.valueChanged)
-        let controller = getDateAlert(datePicker: datePicker)
-        present(controller, animated: true, completion: nil)
-    }
-    
     @IBAction func createEventButtonPressed(_ sender: Any) {
-        newEvent.setBasic(name: eventNameTextField.text, location: locationLabel.text, group: groupButton.title(for: .normal), description: descriptionTextField.text, duration: durationTextField.text ?? "")
+        newEvent.setBasic(name: eventNameTextField.text, location: locationLabel.text, group: groupButton.title(for: .normal), description: descriptionTextField.text, duration: durationButton.title(for: .normal) ?? "")
+        newEvent.setDays(mon: mondaySwitch.isOn, tue: tuesdaySwitch.isOn, wed: wednesdaySwitch.isOn, thu: thursdaySwitch.isOn, fri: fridaySwitch.isOn, sat: saturdaySwitch.isOn, sun: sundaySwitch.isOn)
         
         let errorMessage = newEvent.validateEvent()
         if errorMessage != "" {
@@ -97,47 +133,6 @@ class CreateEventViewController: UIViewController, LocationChanger {
             present(controller,animated: true)
         }
         
-    }
-    
-    // MARK: - Make date alert scroller
-    func getDateAlert(datePicker: UIDatePicker) -> UIAlertController {
-        let alertController = UIAlertController(title: "\n\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
-        
-        datePicker.preferredDatePickerStyle = .wheels
-        alertController.view.addSubview(datePicker)
-        
-        alertController.addAction(UIAlertAction(title: "Select", style: .default) { _ in
-            print("Selected Date: \(datePicker.date)")
-        })
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
-        return alertController
-    }
-    
-    // MARK: - Date Changers
-    
-    @objc func noEarlierDateChange(datePicker: UIDatePicker){
-        newEvent.noEarlierThan = datePicker.date
-        noEarlierButton.setTitle(formatTime(date: datePicker.date), for: .normal)
-    }
-    
-    @objc func noLaterDateChange(datePicker: UIDatePicker){
-        newEvent.noLaterThan = datePicker.date
-        noLaterButton.setTitle(formatTime(date: datePicker.date), for: .normal)
-    }
-    
-    
-    // MARK: - Formatters
-    func formatTime(date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm"
-        return formatter.string(from: date)
-    }
-    
-    func formatDate(date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MMM dd yyyy"
-        return formatter.string(from: date)
     }
     
     // MARK: - handle segue for location
